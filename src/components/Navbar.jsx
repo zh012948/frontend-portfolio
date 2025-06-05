7a4c884ee7707d8a36397f3520e1e1ff719948fe
Zeeshan <zh012948@gmail.com>
2025-06-05 06:33:33 +0500

    Navbar Created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,38 @@
-import '../styles/Navbar.css'
+import { useState } from 'react';
+import '../styles/Navbar.css';
 
 export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
-        <>
-            <div className="navbar">
-                <div className="logo">
-                    Zeeshan
+        <div className="navbar">
+            <div className="logo">Zeeshan</div>
+
+            {/* Nav Links Section */}
+            <div className={`navlinks ${menuOpen ? 'active' : ''}`}>
+                <ul>
+                    <li><a href="#"><i className='bx bx-home-circle'></i><br />Home</a></li>
+                    <li><a href="#"><i className='bx bx-user-circle'></i><br />About</a></li>
+                    <li><a href="#"><i className='bx bx-file'></i><br />Skills</a></li>
+                    <li><a href="#"><i className='bx bx-briefcase-alt-2'></i><br />Services</a></li>
+                    <li><a href="#"><i className='bx bx-image'></i><br />Portfolio</a></li>
+                    <li><a href="#"><i className='bx bx-send'></i><br />Contact Me</a></li>
+                </ul>
+
+
+            </div>
+
+            {/* Top Right Icons */}
+            <div className="icons">
+                <div className="display-mode">
+                    <i className='bx bx-moon'></i>
                 </div>
 
-                <div className="navlinks">
-                    <ul>
-
-                        <li>
-                            <a href="#">< i class='bxr  bx-home-circle' style={{ color: '#ffffff' }}></i>
-                                <br />Home</a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                < i class='bxr  bx-user-circle' style={{ color: '#ffffff' }}></i><br />
-                                About</a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                < i class='bxr  bx-file-detail' style={{ color: '#ffffff' }}></i><br />
-                                Skills</a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                < i class='bxr  bx-briefcase-alt-2' style={{ color: '#ffffff' }}></i><br />
-                                Services</a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                < i class='bxr  bx-image' style={{ color: '#ffffff' }}></i><br />
-                                Portfolio</a>
-                        </li>
-
-                        <li>
-                            <a href="#">
-                                < i className='bxr  bx-send-alt-2' style={{ color: '#ffffff' }}></i><br />
-                                Contact Me</a>
-                        </li>
-
-                    </ul>
+                {/* Menu Icon Toggle */}
+                <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+                    <i className={`bx ${menuOpen ? 'bx-x' : 'bx-grid'}`}></i>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }

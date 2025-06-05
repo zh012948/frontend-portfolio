d686cd79d90f49f16c00294851f42052ee9f9d40
Zeeshan <zh012948@gmail.com>
2025-06-05 05:05:26 +0500

    Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,52 @@ import '../styles/Navbar.css'
 export default function Navbar() {
     return (
         <>
-            This is Navbar
+            <div className="navbar">
+                <div className="logo">
+                    Zeeshan
+                </div>
+
+                <div className="navlinks">
+                    <ul>
+
+                        <li>
+                            <a href="#">< i class='bxr  bx-home-circle' style={{ color: '#ffffff' }}></i>
+                                <br />Home</a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                < i class='bxr  bx-user-circle' style={{ color: '#ffffff' }}></i><br />
+                                About</a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                < i class='bxr  bx-file-detail' style={{ color: '#ffffff' }}></i><br />
+                                Skills</a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                < i class='bxr  bx-briefcase-alt-2' style={{ color: '#ffffff' }}></i><br />
+                                Services</a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                < i class='bxr  bx-image' style={{ color: '#ffffff' }}></i><br />
+                                Portfolio</a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                < i className='bxr  bx-send-alt-2' style={{ color: '#ffffff' }}></i><br />
+                                Contact Me</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }

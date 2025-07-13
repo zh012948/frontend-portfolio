3b6c81bb7dff7d349cdb56707261c340a509793e
Zeeshan <zh012948@gmail.com>
2025-07-13 18:34:08 +0500

    MERN Stack Developer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,34 @@ export default function About() {
         <>
             <div className="about-section">
 
-                About Me
+                <h1 className="heading">About Me</h1>
 
+                <div className="about-container">
+
+                    <div className="about-content">
+                        <p className="role">
+                            Passionate Web devloper
+                        </p>
+
+                        <p className="content">
+                            I specialize in building responsive, accessible, and high-performance web applications using the MERN stack and modern development tools.
+                            <br />
+                            I’m passionate about transforming real-world challenges into practical, impactful solutions through clean code, intuitive design, and robust development practices.
+                        </p>
+
+                        <button className="btnGetInTouch">
+                            Get in Touch
+                        </button>
+
+                        <button className="btnResume">
+                            Download CV
+                        </button>
+                    </div>
+
+                    <div className="about-boxes">
+
+                    </div>
+                </div>
             </div>
         </>
 

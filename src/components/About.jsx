ee355851c1ea7c4853593885842cbfbb4b871f44
Zeeshan <zh012948@gmail.com>
2025-07-17 21:13:15 +0500

    MERN Stack Developer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,15 +9,20 @@ export default function About() {
 
                 <div className="about-container">
 
+
                     <div className="about-content">
                         <p className="role">
                             Passionate Web devloper
                         </p>
 
                         <p className="content">
-                            I specialize in building responsive, accessible, and high-performance web applications using the MERN stack and modern development tools.
-                            <br />
-                            I’m passionate about transforming real-world challenges into practical, impactful solutions through clean code, intuitive design, and robust development practices.
+                            I am a passionate and fast-learning MERN stack developer with a strong focus on front-end development using React. I thrive in collaborative environments, enjoy solving real-world problems with clean, efficient code, and strive to build user-friendly, performance-optimized web applications.
+
+                            My goal is to continuously grow as a developer by working with innovative teams and companies that value quality, creativity, and continuous improvement. I’m particularly driven by the opportunity to transform complex challenges into intuitive, scalable solutions through modern development practices and thoughtful design.
+
+                            Whether it’s enhancing UI/UX, integrating robust backend systems, or optimizing application performance, I am committed to delivering high-quality results that contribute real value. I’m always eager to learn new technologies, adapt to evolving industry trends, and make a meaningful impact wherever I work.
+
+
                         </p>
 
                         <button className="btnGetInTouch">
@@ -25,7 +30,7 @@ export default function About() {
                         </button>
 
                         <button className="btnResume">
-                            Download CV
+                            Check Resume
                         </button>
                     </div>
 

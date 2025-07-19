ec6002bab01aed4859d0b93f63e26eec6ff0ae82
Zeeshan <zh012948@gmail.com>
2025-07-19 15:50:45 +0500

    Skill Section

--- src/components/SkillCard1.jsx
+++ src/components/SkillCard1.jsx
@@ -1,6 +1,12 @@
 import '../styles/SkillCard1.css'
 import dev from '../assets/icons/dev.png'
 import react from '../assets/icons/react.svg'
+import next from '../assets/icons/next.png'
+import ts from '../assets/icons/typescript.png'
+import tailwind from '../assets/icons/tailwind.png'
+import html from '../assets/icons/html-5.png'
+import css from '../assets/icons/css-3.png'
+
 export default function SkillCard1() {
     return (
         <>
@@ -26,20 +32,31 @@ export default function SkillCard1() {
                         <p className="box-title">React</p>
                     </div>
 
-                    <div className="skill-box">
-                        <img src={react} className='box-img' />
-                        <p className="box-title">React</p>
+                    <div className="skill-box ">
+                        <img src={next} className='box-img ' id='next-icon' />
+                        <p className="box-title">Next.js</p>
                     </div>
 
                     <div className="skill-box">
-                        <img src={react} className='box-img' />
-                        <p className="box-title">React</p>
+                        <img src={ts} className='box-img' />
+                        <p className="box-title">TypeScript</p>
                     </div>
 
 
                     <div className="skill-box">
-                        <img src={react} className='box-img' />
-                        <p className="box-title">React</p>
+                        <img src={tailwind} className='box-img' />
+                        <p className="box-title">Tailwind CSS</p>
+                    </div>
+
+
+                    <div className="skill-box">
+                        <img src={html} className='box-img' />
+                        <p className="box-title">HTML 5</p>
+                    </div>
+
+                    <div className="skill-box">
+                        <img src={css} className='box-img' />
+                        <p className="box-title">CSS 3</p>
                     </div>
 
                 </div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,11 +4,18 @@ import SkillCard1 from './SkillCard1'
 export default function Skills() {
     return (
         <>
+
             <div className="skill-section">
                 <h1 className="heading">My Skills</h1>
 
                 <div className="skill-container">
                     <SkillCard1 />
+                    <SkillCard1 />
+                    <SkillCard1 />
+                    <SkillCard1 />
+                    <SkillCard1 />
+                    <SkillCard1 />
+
                 </div>
             </div>
         </>
